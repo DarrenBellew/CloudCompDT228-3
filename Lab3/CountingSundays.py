806ffb15f83036097b225b171d114283721b101a
DarrenBellew <islarf6546@gmail.com>
2015-09-30 16:17:02 +0000

    just finished CountingSundays

--- Lab3/CountingSundays.py
+++ Lab3/CountingSundays.py
@@ -1,6 +1,21 @@
-import datetime
+'''import datetime
 
 daytime.MINYEAR = 1901
 daytime.MAXYEAR = 2000
 
-print(daytime.MAXYEAR)
+print(daytime.MAXYEAR)'''
+import calendar
+
+count = 0
+
+year = 1901
+endYear = 2001
+month = 12
+
+for x in range (year, endYear):
+  for y in range (1, month+1):
+    if calendar.weekday(x,y,1) == calendar.SUNDAY:
+      count = count+1
+
+print("Count: " + str(count))
+
