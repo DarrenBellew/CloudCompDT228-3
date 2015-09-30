c33d35f08adfc68e180bf7cd44ee970f1cf34729
DarrenBellew <islarf6546@gmail.com>
2015-09-30 14:49:00 +0000

    Made the palindrome method

--- PalindromeInput.py
+++ PalindromeInput.py
@@ -1,3 +1,17 @@
 #Palindrome of input
 #Darren Bellew
 #Lab 3
+
+
+def palindrome(word):
+  if(word[::-1] == word):
+    return True
+  return False
+
+
+userInput = raw_input("Please input string to be tested: ")
+if palindrome(userInput):
+  print(userInput + " is a palindrome.")
+else:
+  print(userInput + " is not a palindrome.")
+
