b9a0add3adfc187d798f7f87347ae37ec176f018
DarrenBellew <islarf6546@gmail.com>
2015-09-30 15:47:36 +0000

    practicing import datetime

--- Lab3/CountingSundays.py
+++ Lab3/CountingSundays.py
@@ -1,9 +1,6 @@
-#CountingSundays
+import datetime
 
-#Sundays, 1 Jan 1901 - 31 dec 2000
+daytime.MINYEAR = 1901
+daytime.MAXYEAR = 2000
 
-date = [1,1,1901]
-months = [1,31,2,28,3,31,4,,30,5,,6,,7,,8,,9,,10,,11,,12,]
-
-
-for i in range(date[i], )
+print(daytime.MAXYEAR)
