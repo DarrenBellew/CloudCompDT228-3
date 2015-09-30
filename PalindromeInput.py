7d3f5c6654f1f7bb161ad92d5e9b1a047d46381f
DarrenBellew <islarf6546@gmail.com>
2015-09-30 14:49:51 +0000

    Adding the main code - Complete

--- PalindromeInput.py
+++ PalindromeInput.py
@@ -2,16 +2,15 @@
 #Darren Bellew
 #Lab 3
 
-
+#Function to do python
 def palindrome(word):
   if(word[::-1] == word):
     return True
   return False
 
-
+#Main Code
 userInput = raw_input("Please input string to be tested: ")
 if palindrome(userInput):
   print(userInput + " is a palindrome.")
 else:
   print(userInput + " is not a palindrome.")
-
